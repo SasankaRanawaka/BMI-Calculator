f7e5fbdc72bd4c14f79c4f4bc681952ae654cf86
Sasanka Ranawaka <sasankadinith0@gmail.com>
2024-12-24 22:40:44 -0800

    Add files via upload

---
+++ BMI Calculator/src/javaapplication1/BMICalculator.java
@@ -0,0 +1,16 @@
+
+package javaApplication1;
+
+import javaapplication1.Bmi_calculator;
+
+
+        
+public class BMICalculator {
+    
+ 
+    public static void main(String[] args) {
+        Bmi_calculator s=new Bmi_calculator();
+        s.setVisible(true);
+     
+    }
+}
